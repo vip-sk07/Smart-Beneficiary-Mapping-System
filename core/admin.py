fecd1cb0356a7ae686264a3abf6274e89603b5af
Karan Raj <karanraj2006rk@gmail.com>
2026-02-24 23:51:23 +0530

    Admin fixes and added python-dotenv to requirements

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Category, CustomUser, UserCategories, Scheme, UserEligibility,RuleEngine
+from .models import Category, CustomUser, UserCategories, Scheme, UserEligibility, RuleEngine, Application, Grievance
 
 admin.site.register(Category)
 admin.site.register(CustomUser)
@@ -7,3 +7,5 @@ admin.site.register(UserCategories)
 admin.site.register(Scheme)
 admin.site.register(UserEligibility)
 admin.site.register(RuleEngine)
+admin.site.register(Application)
+admin.site.register(Grievance)
